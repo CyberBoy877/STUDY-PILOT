aee2d91a6130fa5526bb16d44d3f350551b27f37
CyberBoy877 <arihant24483.dpskalyanpur@gmail.com>
2026-06-07 14:20:46 +0530

    Add service worker for caching assets
    Implement service worker caching for assets.

--- sw.js
+++ sw.js
@@ -1,1 +1,23 @@
+const CACHE_NAME = "studypilot-v1";
 
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll([
+                "./",
+                "./index.html",
+                "./style.css",
+                "./script.js",
+                "./videos.json"
+            ]);
+        })
+    );
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
