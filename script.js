669809641f85cf458174f6d53a53bfef885b57ea
CyberBoy877 <arihant24483.dpskalyanpur@gmail.com>
2026-06-07 14:39:58 +0530

    Register service worker on window load

--- script.js
+++ script.js
@@ -220,3 +220,14 @@ window.onload = () => {
     updateTimerDisplay();
     loadVideo();
 };
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", () => {
+        navigator.serviceWorker.register("./sw.js")
+            .then(() => {
+                console.log("Service Worker Registered");
+            })
+            .catch((err) => {
+                console.log("SW failed", err);
+            });
+    });
+}
