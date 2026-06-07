95a726bc1b6e8dc2cc041e423b2ccc0edfc6b147
CyberBoy877 <arihant24483.dpskalyanpur@gmail.com>
2026-06-07 14:38:50 +0530

    Refactor service worker to use FILES_TO_CACHE array

--- sw.js
+++ sw.js
@@ -1,23 +1,26 @@
 const CACHE_NAME = "studypilot-v1";
 
-self.addEventListener("install", event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then(cache => {
-            return cache.addAll([
-                "./",
-                "./index.html",
-                "./style.css",
-                "./script.js",
-                "./videos.json"
-            ]);
-        })
-    );
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./videos.json",
+  "./subjects.json"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
 });
 
-self.addEventListener("fetch", event => {
-    event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request);
-        })
-    );
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
